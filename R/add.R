fd9d825a1b1653429a7d3668b18b7cb1df045f98
Yao <yhz@YaodeMacBook-Air.local>
2023-08-02 03:22:44 +0800

    0802

---
+++ R/add.R
@@ -0,0 +1,22 @@
+
+#' @title Add Two Numbers
+#' @description XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#'     YYYYYYYYYYYYYYYYYYYYYYYYYYYYY
+#'     ZZZZZZZZZZZZZZZZZZZZZZZZ.
+#'
+#' @param x one number
+#' @param y one number \code{\link[base]{sum}}
+#'
+#' @return sum of two numbers
+#' @export
+#' @importFrom base sum
+#'
+#' @examples sum(1, 2)
+#' \donttest{
+#' sum(3, 4)}
+#'
+#'
+add <- function(x, y){
+  x + y
+}
+
